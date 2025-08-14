4181eb99abb553a21943de2f96765dd7ae2f5e5e
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-14 16:17:16 -0300

    aprendi o layout e view

---
+++ hdcevents/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('title', 'paivaAdmin')
+
+@section('header')
+    <h1>Bem vindo!</h1>
+    <p>Uma página de teste!</p>
+@endsection  <!-- <-- fechou o header -->
+
+@section('main')
+    <h2>Oi</h2>
+@endsection
