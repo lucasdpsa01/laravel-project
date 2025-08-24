b642171cdc5a7f7f63272117c1c1d29a4ef030f5
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-23 21:11:04 -0300

    doidera

--- hdcevents/resources/views/layout/app.blade.php
+++ hdcevents/resources/views/layout/app.blade.php
@@ -10,10 +10,6 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header>
-            @yield('main')
-        </header>
-
         <main>
             @yield('content')
         </main>
